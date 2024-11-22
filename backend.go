a72ac5341a2783807ef7d342183e7f1b468911fd
Florine0928 <florine0928@gmail.com>
2024-11-22 17:51:57 +0200

    gofmt: everything

--- backend.go
+++ backend.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "encoding/json"
-    es "github.com/furiousman59/GO59" // es.ExecShell es.StringRNG es.FileExists es.Join es.Read
+	"encoding/json"
+	"fmt"
+	es "github.com/furiousman59/GO59" // es.ExecShell es.StringRNG es.FileExists es.Join es.Read
+	"os"
 )
 
 func Usage() {
@@ -15,80 +15,79 @@ func Usage() {
 	fmt.Println("	-bar, -b <etc>				Which Bar you use")
 	fmt.Println("	-scheme, -m <dark/light>		Colorscheme")
 	fmt.Println("	-reload, -r				Refreshes Components such as bar")
-    fmt.Println("	-disable, -d				Disable a component")
+	fmt.Println("	-disable, -d				Disable a component")
 }
 
 func CacheDir() {
-    if es.FileExists(cache) == false {
-        err := os.MkdirAll(cache, 0755)
-        if err != nil {
-        fmt.Println("Error creating cache directory:", err)
-        os.Exit(1)
-    }
-    if !es.FileExists(cachewp) {
-        file, err := os.Create(cachewp)
-        if err != nil {
-            fmt.Println("Error creating cache file:", err)
-            return
-        }
-        defer file.Close()
-        fmt.Println("Cache file created successfully")
-    } else {
-        fmt.Println("Cache file already exists")
-    }
+	if es.FileExists(cache) == false {
+		err := os.MkdirAll(cache, 0755)
+		if err != nil {
+			fmt.Println("Error creating cache directory:", err)
+			os.Exit(1)
+		}
+		if !es.FileExists(cachewp) {
+			file, err := os.Create(cachewp)
+			if err != nil {
+				fmt.Println("Error creating cache file:", err)
+				return
+			}
+			defer file.Close()
+			fmt.Println("Cache file created successfully")
+		} else {
+			fmt.Println("Cache file already exists")
+		}
+	}
 }
-    }
 
-var cache string // ~/.cache/gopherconf               
-var cachewp string  // cached wallpaper
+var cache string   // ~/.cache/gopherconf
+var cachewp string // cached wallpaper
 var SESSION = os.Getenv("XDG_SESSION_TYPE")
 var util string
 
 func init() {
-    home, err := os.UserHomeDir()
-    if err != nil {
-        fmt.Println("Error fetching home directory:", err)
-        os.Exit(1)
-    }
-    cache = es.Join(home, ".cache", "gopherconf")
-    cachewp = cache + "/" + "CACHED_WALLPAPER"
-    CacheDir()
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error fetching home directory:", err)
+		os.Exit(1)
+	}
+	cache = es.Join(home, ".cache", "gopherconf")
+	cachewp = cache + "/" + "CACHED_WALLPAPER"
+	CacheDir()
 
-    if SESSION == "wayland" {
-        util = "swaybg"
-    } else if SESSION == "x11" {
-        util = "feh"
-    }
+	if SESSION == "wayland" {
+		util = "swaybg"
+	} else if SESSION == "x11" {
+		util = "feh"
+	}
 
 }
 
 func FuncUtil() {
-    if SESSION == "wayland" {
-        KillBar()
-        es.ExecShell("nohup", true, "sh", "-c", util+" -i "+config.Wallpaper+" > /dev/null 2>&1 &")
-    } else if SESSION == "x11" {
-        KillBar()
-        es.ExecShell("nohup", true, "sh", "-c", util+" --bg-scale "+config.Wallpaper+" > /dev/null 2>&1 &")
-    } else {
-        fmt.Println("Unknown session type:", SESSION)
-    }
+	if SESSION == "wayland" {
+		KillBar()
+		es.ExecShell("nohup", true, "sh", "-c", util+" -i "+config.Wallpaper+" > /dev/null 2>&1 &")
+	} else if SESSION == "x11" {
+		KillBar()
+		es.ExecShell("nohup", true, "sh", "-c", util+" --bg-scale "+config.Wallpaper+" > /dev/null 2>&1 &")
+	} else {
+		fmt.Println("Unknown session type:", SESSION)
+	}
 }
 
 func KillBar() {
-    es.ExecShell("pkill", true, util)
+	es.ExecShell("pkill", true, util)
 }
 
 func WriteConfig() {
-    configPath := es.Join(cache, "config.json")
-    b, err := json.Marshal(config)
-    if err != nil {
-        fmt.Println("Error: Could not marshal configuration:", err)
-        return
-    }
-    err = os.WriteFile(configPath, b, 0644)
-    if err != nil {
-        fmt.Println("Error: Could not write configuration:", err)
-        return
-    }
-}   
-
+	configPath := es.Join(cache, "config.json")
+	b, err := json.Marshal(config)
+	if err != nil {
+		fmt.Println("Error: Could not marshal configuration:", err)
+		return
+	}
+	err = os.WriteFile(configPath, b, 0644)
+	if err != nil {
+		fmt.Println("Error: Could not write configuration:", err)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,69 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
-    "encoding/json"
 )
 
 var config struct { // config
-    Wallpaper string `json:"wallpaper"`
-    Util string `json:"util"`
+	Wallpaper string `json:"wallpaper"`
+	Util      string `json:"util"`
 }
 
 func main() {
-    configPath := filepath.Join(cache, "config.json")
-    b, err := os.ReadFile(configPath)
-    if err != nil {
-        config = struct {
-            Wallpaper string `json:"wallpaper"`
-            Util string `json:"util"`
-        }{}
-    } else {
-        err = json.Unmarshal(b, &config)
-        if err != nil {
-            fmt.Println("Error: Could not unmarshal configuration:", err)
-            return
-        }
-    }
+	configPath := filepath.Join(cache, "config.json")
+	b, err := os.ReadFile(configPath)
+	if err != nil {
+		config = struct {
+			Wallpaper string `json:"wallpaper"`
+			Util      string `json:"util"`
+		}{}
+	} else {
+		err = json.Unmarshal(b, &config)
+		if err != nil {
+			fmt.Println("Error: Could not unmarshal configuration:", err)
+			return
+		}
+	}
 
-    args := os.Args[1:]
-    for i := 0; i < len(args); i++ {
-        arg := args[i]
-        switch arg {
-        case "-help", "-h":
-            Usage()
-        case "-wallpaper", "-w":
-            if i+1 < len(args) {
-                config.Wallpaper = args[i+1]
-                if config.Util == "disabled" {
-                    config.Util = "enabled"
-                }
-                WriteConfig()
-                FuncUtil()
-                fmt.Println("Wallpaper Path set to:", config.Wallpaper)
-                i++
-            } else if config.Wallpaper != "" {
-                FuncUtil()
-                fmt.Println("Cached Wallpaper:", config.Wallpaper)
-            } else {
-                fmt.Println("Error: No wallpaper path provided")
-                return
-            }
-        case "-disable", "-d":
-            if i+1 < len(args) {
-                if args[i+1] == "wallpaper" {
-                    config.Util = "disabled"
-                    WriteConfig()
-                    KillBar()
-                    fmt.Println(util, "has been disabled")
-                    i++
-                }
-            }
-        default:
-            fmt.Printf("Unknown operation: %s\n", arg)
-        }
-    }
+	args := os.Args[1:]
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		switch arg {
+		case "-help", "-h":
+			Usage()
+		case "-wallpaper", "-w":
+			if i+1 < len(args) {
+				config.Wallpaper = args[i+1]
+				if config.Util == "disabled" {
+					config.Util = "enabled"
+				}
+				WriteConfig()
+				FuncUtil()
+				fmt.Println("Wallpaper Path set to:", config.Wallpaper)
+				i++
+			} else if config.Wallpaper != "" {
+				FuncUtil()
+				fmt.Println("Cached Wallpaper:", config.Wallpaper)
+			} else {
+				fmt.Println("Error: No wallpaper path provided")
+				return
+			}
+		case "-disable", "-d":
+			if i+1 < len(args) {
+				if args[i+1] == "wallpaper" {
+					config.Util = "disabled"
+					WriteConfig()
+					KillBar()
+					fmt.Println(util, "has been disabled")
+					i++
+				}
+			}
+		default:
+			fmt.Printf("Unknown operation: %s\n", arg)
+		}
+	}
 }
-
